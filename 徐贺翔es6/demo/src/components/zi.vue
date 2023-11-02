0d619d47623d05077fd80dbe5e3ade77aaf1481e
xuhexiang5 <3085547994@qq.com>
2023-11-02 10:08:22 +0800

    file:上传第一次代码

---
+++ 徐贺翔es6/demo/src/components/zi.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>{{ b }}</h2>
+
+    </div>
+</template>
+
+<script>
+export default {
+   props:['b'],
+    data() {
+        return {
+           
+        };
+    },
+    methods: {},
+    computed: {},
+    filters: {},
+    watch: {}
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ 徐贺翔es6/demo/src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+  <button @click="cun">存</button>
+  <button @click="qu">取</button>
+  <button @click="shan">删</button>
+  <h2>{{ this.a }}</h2>
+  ——————————————————————————————————————
+  <zi :b="b"></zi>
+  <button @click="zii">加</button>
+  <button @click="jian">减</button>
+  </div>
+</template>
+
+<script>
+import  zi  from '../components/zi.vue' ;
+export default {
+ data() {
+   return {
+    a:'哈',
+    b:1
+   };
+ },
+ methods: {
+  cun(){
+    localStorage.setItem('list', '哈哈哈')
+  },
+  qu(){
+   this.a=localStorage.getItem('list')
+  },
+  shan(){
+    localStorage.clear()
+  },
+  zii(){
+    this.b++
+  },
+  jian(){
+    this.b--
+  }
+ },
+ computed: {},
+ filters: {},
+ watch: {},
+ components:{
+  zi
+ }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
